4ebec0207192661277467328e2ec47f7bdb57047
Ju4tCode <42488585+yanyongyu@users.noreply.github.com>
2024-03-18 08:25:07 -0400

    :bug: fix example chatbot current time zone

--- examples/chatbot/custom.py
+++ examples/chatbot/custom.py
@@ -3,4 +3,4 @@ from datetime import datetime, timezone
 
 async def current_time() -> str:
     """Get the current real world time."""
-    return datetime.now(timezone.utc).isoformat()
+    return datetime.now(timezone.utc).astimezone().isoformat()
