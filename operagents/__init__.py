89f44c6fca4a6c5522b9af873d7f60be4ad14c39
Ju4tCode <42488585+yanyongyu@users.noreply.github.com>
2024-04-16 03:11:24 -0400

    :sparkles: add validate cli command

--- operagents/__init__.py
+++ operagents/__init__.py
@@ -1,2 +1,6 @@
+from .log import logger as logger
 from .opera import Opera as Opera
+from .version import VERSION as VERSION
 from .config import OperagentsConfig as OperagentsConfig
+
+__version__ = VERSION
--- operagents/cli/__init__.py
+++ operagents/cli/__init__.py
@@ -5,12 +5,15 @@ from pathlib import Path
 from typing import Literal
 
 import yaml
+from pydantic import ValidationError
 
 from operagents.opera import Opera
+from operagents.version import VERSION
 from operagents.config import OperagentsConfig
 from operagents.log import logger, setup_logging
 
 parser = argparse.ArgumentParser(prog="operagents", description="OperAgents CLI")
+parser.add_argument("--version", "-V", action="version", version=VERSION)
 
 subcommands = parser.add_subparsers(title="Commands")
 
@@ -51,6 +54,54 @@ run.add_argument(
 )
 run.add_argument("config", help="The path to the operagents configuration file.")
 run.set_defaults(handler=handle_run)
+
+
+async def handle_validate(
+    config: str, path: bool = True, log_level: Literal["DEBUG", "INFO"] = "INFO"
+):
+    setup_logging(log_level)
+
+    if path:
+        sys_path = str(Path.cwd().resolve())
+        if sys_path not in sys.path:
+            sys.path.insert(0, sys_path)
+
+    try:
+        opera_config = OperagentsConfig.model_validate(
+            yaml.safe_load(Path(config).read_text())
+        )
+    except ValidationError as e:
+        logger.error(f"Config file is invalid.\n{e}")
+        exit(1)
+    except Exception:
+        logger.opt(exception=True).error("Config file is invalid.")
+        exit(1)
+
+    try:
+        Opera.from_config(opera_config)
+    except Exception:
+        logger.opt(exception=True).exception("Failed to load opera config.")
+        exit(1)
+
+    logger.info("Config file is valid.")
+
+
+validate = subcommands.add_parser(
+    "validate",
+    help="Validate the operagents config.",
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+)
+validate.add_argument(
+    "--path",
+    default=True,
+    action=argparse.BooleanOptionalAction,
+    help="Add current path to sys.path.",
+)
+validate.add_argument(
+    "--log-level", default="INFO", choices=["DEBUG", "INFO"], help="The log level."
+)
+validate.add_argument("config", help="The path to the operagents configuration file.")
+validate.set_defaults(handler=handle_validate)
 
 
 def main():
